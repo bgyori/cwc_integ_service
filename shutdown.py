6c0991fa524ca9abb705a1134e832448950d47a2
pagreene <patrick.anton.greene@gmail.com>
2018-07-26 20:38:15 +0000

    Actually remove the containers.

--- shutdown.py
+++ shutdown.py
@@ -12,6 +12,7 @@ def shutdown():
         print("(%d/%d) Resolving %s...." % (i+1, len(cont_list), cont.name))
         get_logs_for_container(cont)
         cont.stop()
+        cont.remove()
     print("+" + "-"*78 + "+")
     print("| %-76s |" % "All done! Have a nice day! :)")
     print("+" + "-"*78 + "+")
