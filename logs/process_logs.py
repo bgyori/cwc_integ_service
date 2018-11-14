22f280cbb6e027e2c8acd176528b4665b4415bd8
pagreene <patrick.anton.greene@gmail.com>
2018-11-14 12:36:56 -0500

    Refactor to reduce redundancy.

--- logs/process_logs.py
+++ logs/process_logs.py
@@ -12,30 +12,118 @@ from kqml import *
 THIS_DIR = os.path.abspath(os.path.dirname(__file__))
 
 
-class Message(object):
+def format_sys_utterance(entry):
+    msg_text = entry.content.get('content').gets('what')
+    html = """
+    <div class="row sys_utterance" style="margin-top: 15px">
+      <div class="col-sm sys_name">
+        <span style="background-color:#2E64FE; color: 
+        #FFFFFF">Bob:</span>&nbsp;<a style="color: #BDBDBD">{time}</a>
+      </div>
+    </div>
+
+    <div class="row sys_utterance" style="margin-bottom: 15px">
+      <div class="col-sm sys_msg">{txt}</div>
+    </div>
+    """.format(time=entry.time, txt=msg_text)
+    return textwrap.dedent(html)
+
+
+def format_user_utterance(entry):
+    msg_text = entry.content.get('content').gets('text')
+    html = """
+    <div class="row usr_utterance" style="margin-top: 15px">
+      <div class="col-sm usr_name">
+        <span style="background-color: #A5DF00; color: 
+        #FFFFFF">User:</span>&nbsp;<a style="color: #BDBDBD">{time}</a>
+      </div>
+    </div>
+
+    <div class="row usr_utterance" style="margin-bottom: 15px">
+      <div class="col-sm usr_msg">{txt}</div>
+    </div>
+    """.format(time=entry.time, txt=msg_text)
+    return textwrap.dedent(html)
+
+
+def format_provenance(entry):
+    prov_html = entry.content.get('content').gets('html')
+    html = """
+    <div class="row add_provenance" style="margin-top: 15px">
+        <div class="col-sm sys_name">
+            <span style="background-color: #2E64FE; color: 
+            #FFFFFF">Bob (provenance):</span>&nbsp;<a style="color:
+            #BDBDBD">{time}</a>
+        </div>
+    </div>
+
+    {provenance_html}
+    """.format(time=entry.time, provenance_html=prov_html)
+    return textwrap.dedent(html)
+
+
+def make_html(html_parts):
+    with open(os.path.join(THIS_DIR, 'page_template.html'), 'r') as fh:
+        template = fh.read()
+
+    html = template.replace('%%%CONTENT%%%', '\n'.join(html_parts))
+    return html
+
+
+def format_start_time(start_time):
+    html = """
+    <div class="row start_time">
+      <div class="col-sm">Dialogue started at: {start_time}</div>
+    </div>
+    """.format(start_time=start_time)
+    return textwrap.dedent(html)
+
+
+class CwcLogEntry(object):
+    """Parent class for entries in the logs."""
     possible_sems = ('sys_utterance', 'user_utterance', 'display_image',
                      'add_provenance', 'display_sbgn')
 
-    def __init__(self, type, partner, time, content):
+    def __init__(self, type, time, message, partner):
         self.type = type
-        self.partner = partner
         self.time = time
-        self.content = content
+        self.message = message
+        self.partner = partner
+        self.content = None
         self.sem = None
-        for sem in self.possible_sems:
-            if self.content_is(sem):
-                self.sem = sem
-                break
         return
+
+    def get_content(self):
+        """Convert the entry to a message."""
+        if not self.content:
+            self.content = KQMLPerformative.from_string(self.message)
+        return self.content
+
+    def get_sem(self):
+        """Get the sem (semantic) of this entry."""
+        if not self.sem:
+            for sem in self.possible_sems:
+                if self._content_is(sem):
+                    self.sem = sem
+                    break
+        return self.sem
+
+    def is_sem(self, sem):
+        """Check whether this entry matches the given sem."""
+        if sem not in self.possible_sems:
+            raise ValueError("Invalid sem: %s" % sem)
+        return self.get_sem() == sem
 
     def _cont_is_type(self, head, content_head):
         try:
-            return (self.content.head().upper() == head.upper() and
-                    self.content.get('content').head().upper() == content_head.upper())
+            my_head = self.content.head()
+            my_content_head = self.content.get('content').head()
+            return (my_head.upper() == head.upper() and
+                    my_content_head.upper() == content_head.upper())
         except Exception as e:
             return False
 
-    def content_is(self, msg_type):
+    def _content_is(self, msg_type):
         simple_types = ['display_image', 'display_sbgn', 'add_provenance']
         if msg_type in simple_types:
             msg_type = msg_type.replace('_', '-')
@@ -56,107 +144,6 @@ class Message(object):
             return False
         logger.warning("Unrecognized message type: %s" % msg_type)
         return False
-
-
-def format_sys_utterance(msg):
-    msg_text = msg.content.get('content').gets('what')
-    html = """
-    <div class="row sys_utterance" style="margin-top: 15px">
-      <div class="col-sm sys_name">
-        <span style="background-color:#2E64FE; color: 
-        #FFFFFF">Bob:</span>&nbsp;<a style="color: #BDBDBD">{time}</a>
-      </div>
-    </div>
-
-    <div class="row sys_utterance" style="margin-bottom: 15px">
-      <div class="col-sm sys_msg">{txt}</div>
-    </div>
-    """.format(time=msg.time, txt=msg_text)
-    return textwrap.dedent(html)
-
-
-def format_user_utterance(msg):
-    msg_text = msg.content.get('content').gets('text')
-    html = """
-    <div class="row usr_utterance" style="margin-top: 15px">
-      <div class="col-sm usr_name">
-        <span style="background-color: #A5DF00; color: 
-        #FFFFFF">User:</span>&nbsp;<a style="color: #BDBDBD">{time}</a>
-      </div>
-    </div>
-
-    <div class="row usr_utterance" style="margin-bottom: 15px">
-      <div class="col-sm usr_msg">{txt}</div>
-    </div>
-    """.format(time=msg.time, txt=msg_text)
-    return textwrap.dedent(html)
-
-
-def format_provenance(msg):
-    prov_html = msg.content.get('content').gets('html')
-    html = """
-    <div class="row add_provenance" style="margin-top: 15px">
-        <div class="col-sm sys_name">
-            <span style="background-color: #2E64FE; color: 
-            #FFFFFF">Bob (provenance):</span>&nbsp;<a style="color:
-            #BDBDBD">{time}</a>
-        </div>
-    </div>
-
-    {provenance_html}
-    """.format(time=msg.time, provenance_html=prov_html)
-    return textwrap.dedent(html)
-
-
-def make_html(html_parts):
-    with open(os.path.join(THIS_DIR, 'page_template.html'), 'r') as fh:
-        template = fh.read()
-
-    html = template.replace('%%%CONTENT%%%', '\n'.join(html_parts))
-    return html
-
-
-def get_io_msgs(log):
-    io_msgs = []
-    entry_list = log.all
-    for entry in entry_list:
-        try:
-            msg = entry.to_message()
-        except Exception:
-            continue
-        if msg.sem is not None:
-            io_msgs.append(msg)
-    return io_msgs
-
-
-def format_start_time(start_time):
-    html = """
-    <div class="row start_time">
-      <div class="col-sm">Dialogue started at: {start_time}</div>
-    </div>
-    """.format(start_time=start_time)
-    return textwrap.dedent(html)
-
-
-class CwcLogEntry(object):
-    """Parent class for entries in the logs."""
-
-    def __init__(self, type, time, message, partner):
-        self.type = type
-        self.time = time
-        self.message = message
-        self.partner = partner
-        self.content = None
-        return
-
-    def to_message(self):
-        """Convert the entry to a message."""
-        if not self.content:
-            self.content = KQMLPerformative.from_string(self.message)
-
-        # Add message semantics
-        msg = Message(self.type, self.partner, self.time, self.content)
-        return msg
 
     def __repr__(self):
         ret = '<%s ' % self.__class__.__name__
@@ -181,25 +168,58 @@ class CwcLog(object):
 
     def __init__(self, log_dir):
         self.log_dir = log_dir
+
+        # Load and parse the log file.
         self.log_file = os.path.join(log_dir, 'log.txt')
         with open(self.log_file, 'r') as f:
             self.__log = f.read()
-        tp = self.time_patt.search(self.__log)
-        assert tp is not None, "Failed to get time string."
-        self.start_time = ' '.join(tp.groups())
-        sec_list = self.section_patt.findall(self.__log)
-        assert sec_list, "Failed to find any sections."
-        self.sent = []
-        self.received = []
-        self.all = []
-        for typ, dt, other_type, partner, msg in sec_list:
-            entry = CwcLogEntry(typ, dt, msg, partner)
-            if typ == 'S':
-                self.sent.append(entry)
-            else:
-                self.received.append(entry)
-            self.all.append(entry)
+        self.start_time = None
+        self.all_entries = None
+
+        # Get familiar with the image stash, if present.
+        self.img_dir = os.path.join(log_dir, 'images')
+        if not os.path.exists(self.img_dir):
+            # If the logs are old and there's no image directory, we'll just
+            # have to live without images.
+            logger.warning("No image directory \"%s\" found. This transcript "
+                           "will have no images included." % self.img_dir)
+            self.img_dir = None
+
+        # This is filled later.
+        self.io_entries = None
         return
+
+    def get_start_time(self):
+        if self.start_time is None:
+            tp = self.time_patt.search(self.__log)
+            assert tp is not None, "Failed to get time string."
+            self.start_time = ' '.join(tp.groups())
+        return self.start_time
+
+    def get_all_entries(self):
+        if self.all_entries is None:
+            self.all_entries = []
+            sec_list = self.section_patt.findall(self.__log)
+            assert sec_list, "Failed to find any sections."
+            for typ, dt, other_type, partner, msg in sec_list:
+                entry = CwcLogEntry(typ, dt, msg, partner)
+                self.all_entries.append(entry)
+        return self.all_entries
+
+    def get_io_entries(self):
+        if not self.io_entries:
+            self.io_entries = []
+            entry_list = self.get_all_entries()
+            for entry in entry_list:
+                try:
+                    entry.get_content()
+                except KQMLException:
+                    logger.debug("Failed to get content for:\n%s."
+                                 % repr(entry))
+                    continue
+                if entry.get_sem() is not None:
+                    self.io_entries.append(entry)
+        return self.io_entries
 
 
 def logs_to_html_file(log_dir_path, html_file=None):
@@ -210,21 +230,20 @@ def logs_to_html_file(log_dir_path, html_file=None):
 
     html_parts = ['<div class="container">']
 
-    start_time = log.start_time
+    start_time = log.get_start_time()
     html_parts.append(format_start_time(start_time))
 
     # Find all messages received by the BA
-    io_msgs = get_io_msgs(log)
-    for msg in io_msgs:
-        if msg.sem == 'sys_utterance':
-            print('SYS: %s' % msg.content)
-            html_parts.append(format_sys_utterance(msg))
-        elif msg.sem == 'user_utterance':
-            print('USER: %s' % msg.content)
-            html_parts.append(format_user_utterance(msg))
-        elif msg.sem == 'add_provenance':
+    for entry in log.get_io_entries():
+        if entry.is_sem('sys_utterance'):
+            print('SYS: %s' % entry.get_content())
+            html_parts.append(format_sys_utterance(entry))
+        elif entry.is_sem('user_utterance'):
+            print('USER: %s' % entry.get_content())
+            html_parts.append(format_user_utterance(entry))
+        elif entry.is_sem('add_provenance'):
             print('SYS: <sent content to provenance>')
-            html_parts.append(format_provenance(msg))
+            html_parts.append(format_provenance(entry))
     html_parts.append('</div>')
 
     with open(html_file, 'w') as fh:
